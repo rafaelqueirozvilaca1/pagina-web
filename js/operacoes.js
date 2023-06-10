4e45349d0e510af2416e168299e58474a689b581
Your Name <you@example.com>
2023-06-09 22:49:00 -0300

    Alteracoes js

--- js/operacoes.js
+++ js/operacoes.js
@@ -14,47 +14,46 @@ let restoDivisao = (a, b) => a % b;
 let botaoResolverEl = document.querySelector('#botao-resolver');
 botaoResolverEl.addEventListener('click', realizandoOperacao);
 function realizandoOperacao() {
-
     if (!primeiroNumeroEl.value || !segundoNumeroEl.value) {
-        alert ('Preencha corretamente todos os campos de entrada');
+        alert('Preencha corretamente todos os campos de entrada');
         return;
     }
 
     let soma = somaNumeros(primeiroNumeroEl.value, segundoNumeroEl.value);
-    let subtracao = subtraiNumeros (primeiroNumero, segundoNumero);
-    let produto = multiplicaDoisNumeros (primeiroNumero, segundoNumero);
-    let divisao = divideDoisNumeros (primeiroNumero, segundoNumero);
-    let resto = restoDivisao (primeiroNumero, segundoNumero);
+    let subtracao = subtraiNumeros(primeiroNumero, segundoNumero);
+    let produto = multiplicaDoisNumeros(primeiroNumero, segundoNumero);
+    let divisao = divideDoisNumeros(primeiroNumero, segundoNumero);
+    let resto = restoDivisao(primeiroNumero, segundoNumero);
 
     switch (opcaoEl.value) {
-        case '+': 
+        case '+':
             resultadoEl.value = `Soma = ${soma}`; // realiza a soma se opção digitada for "+".
             break;
-        case '-': 
+        case '-':
             resultadoEl.value = `Resultado da subtração = ${subtracao}`;
             break;
-        case '*': 
+        case '*':
             resultadoEl.value = `Produto = ${produto}`; // realiza a multiplicação se opção digitada for "*".
             break;
-        case '/': 
+        case '/':
             resultadoEl.value = `Quociente = ${divisao}`; // realiza a divisão se opção digitada for "-".
             break;
-        case '%': 
+        case '%':
             resultadoEl.value = `Resto da divisão = ${resto}`; // realiza a soma se opção digitada for "+".
             break;
-        default: 
-            alert ('Opção inválida'); // se a opção digitada for diferente das mencionadas acima, imprime mensagem de alerta avisando que a opção é inválida.
+        default:
+            alert('Opção inválida'); // se a opção digitada for diferente das mencionadas acima, imprime mensagem de alerta avisando que a opção é inválida.
             return;
     }
     primeiroNumeroEl.focus();
 }
 
-primeiroNumeroEl.addEventListener ('keyup', botaoEnterClicado);
-segundoNumeroEl.addEventListener ('keyup', botaoEnterClicado);
-opcaoEl.addEventListener ('keyup', botaoEnterClicado);
-function botaoEnterClicado (e) {
+primeiroNumeroEl.addEventListener('keyup', botaoEnterClicado);
+segundoNumeroEl.addEventListener('keyup', botaoEnterClicado);
+opcaoEl.addEventListener('keyup', botaoEnterClicado);
+function botaoEnterClicado(e) {
     if (e.key === 'Enter') {
-        realizandoOperacao ();
+        realizandoOperacao();
     }
 }
 
@@ -63,37 +62,37 @@ function botaoEnterClicado (e) {
 // função que conta o número de vezes que o botão "Clique em mim!" foi clicado
 let contaCliques = 0; // inicializa o numero de cliques
 
-function mostrarMensagem (botao, contaCliques) {
+function mostrarMensagem(botao, contaCliques) {
     return botao.innerHTML = `Botão Clique em mim! foi clicado ${contaCliques} vezes`;
 }
 
-let botaoCliqueEmMimEl = document.querySelector ('#botao-clique-em-mim');
-botaoCliqueEmMimEl.addEventListener ('click',  (e) => { 
+let botaoCliqueEmMimEl = document.querySelector('#botao-clique-em-mim');
+botaoCliqueEmMimEl.addEventListener('click', (e) => {
     let botaoRecuperadoEl = e.currentTarget;
     contaCliques++; // conta quantas vezes o botão foi clicado
-    mostrarMensagem (botaoRecuperadoEl, contaCliques); // recupera o botão clicado e altera o seu conteúdo
+    mostrarMensagem(botaoRecuperadoEl, contaCliques); // recupera o botão clicado e altera o seu conteúdo
 });
 
-let botaoClicadoEl = document.querySelector ('#botao-clique-em-mim'); //seleciona o botão "Clique em Mim!"
+let botaoClicadoEl = document.querySelector('#botao-clique-em-mim'); //seleciona o botão "Clique em Mim!"
 
 // função que zera o número de cliques do botão "Clique em mim!"
-let botaoZerarCliquesEl = document.querySelector ('#botao-zerar-cliques');
-botaoZerarCliquesEl.addEventListener ('click', () => {
+let botaoZerarCliquesEl = document.querySelector('#botao-zerar-cliques');
+botaoZerarCliquesEl.addEventListener('click', () => {
     contaCliques = 0; // zera o número de cliques
-    mostrarMensagem (botaoClicadoEl, contaCliques);
+    mostrarMensagem(botaoClicadoEl, contaCliques);
 });
 
 // função que decrementa o número de cliques
-let botaoDecrementarEl = document.querySelector ('#botao-decrementar');
-botaoDecrementarEl.addEventListener ('click', () => {
+let botaoDecrementarEl = document.querySelector('#botao-decrementar');
+botaoDecrementarEl.addEventListener('click', () => {
     contaCliques--; // decrementa o número de cliques
-    mostrarMensagem (botaoClicadoEl, contaCliques);
+    mostrarMensagem(botaoClicadoEl, contaCliques);
 });
 
 // ----------------------------------------------------------------------------------------------------
 
 // mostra o horário e a data atual
-let horarioAtualEl = document.querySelector ('#data-e-hora-atual');
+let horarioAtualEl = document.querySelector('#data-e-hora-atual');
 horarioAtualEl.value = new Date();
 
 //--------------------------------------------------------------------------------------------------------------
@@ -101,8 +100,8 @@ horarioAtualEl.value = new Date();
 //realiza operações com coeficientes 'a', 'b' e 'c' da equação do segundo grau digitados pelo usuário
 
 let calculaDelta = (a, b, c) => b * b - 4 * a * c;
-let calculaRaizPositiva = (b, delta, a) => ((- b + Math.sqrt (delta)) / (2 * a));
-let calculaRaizNegativa = (b, delta, a) => ((- b - Math.sqrt (delta)) / (2 * a));
+let calculaRaizPositiva = (b, delta, a) => ((- b + Math.sqrt(delta)) / (2 * a));
+let calculaRaizNegativa = (b, delta, a) => ((- b - Math.sqrt(delta)) / (2 * a));
 let calculaVerticeX = (b, a) => ((- b) / (2 * a));
 let calculaVerticeY = (delta, a) => ((- delta) / (4 * a));
 
@@ -110,6 +109,7 @@ let aEl = document.querySelector('#coeficiente-a');
 let bEl = document.querySelector('#coeficiente-b');
 let cEl = document.querySelector('#coeficiente-c');
 let opcaoOperacoesEl = document.querySelector('#opcao-equacoes');
+
 let botaoResolverEquacaoEl = document.querySelector('#resolver-equacao');
 botaoResolverEquacaoEl.addEventListener('click', resolvendoEquacoes);
 function resolvendoEquacoes() {
@@ -118,18 +118,18 @@ function resolvendoEquacoes() {
     let b = bEl.value;
     let c = cEl.value;
 
-    let delta = calculaDelta (a, b, c);
-    let raizPositiva = calculaRaizPositiva (b, delta, a);
-    let raizNegativa = calculaRaizNegativa (b, delta, a);
+    let delta = calculaDelta(a, b, c);
+    let raizPositiva = calculaRaizPositiva(b, delta, a);
+    let raizNegativa = calculaRaizNegativa(b, delta, a);
 
-    let verticeX = calculaVerticeX (b, a);
-    let verticeY = calculaVerticeY (delta, a);
+    let verticeX = calculaVerticeX(b, a);
+    let verticeY = calculaVerticeY(delta, a);
 
     let resultadoEquacoesEl = document.querySelector('#resultado-equacoes');
     let opcaoOperacoes = opcaoOperacoesEl.value;
 
     if (!a || !b || !c || !opcaoOperacoes) {
-        alert ('Preencha todos os campos de entrada');
+        alert('Preencha todos os campos de entrada');
     }
 
     else if (a != 0) {
@@ -159,20 +159,20 @@ function resolvendoEquacoes() {
     opcaoOperacoesEl.focus();
 }
 
-let botaoLimparCoeficientesEl = document.querySelector ('#limpar-input');
-botaoLimparCoeficientesEl.addEventListener ('click', () => {
+let botaoLimparCoeficientesEl = document.querySelector('#limpar-input');
+botaoLimparCoeficientesEl.addEventListener('click', () => {
     aEl.value = '';
     bEl.value = '';
     cEl.value = '';
-    opcaoOperacoesEl.focus ();
+    opcaoOperacoesEl.focus();
 });
 
-function botaoEnterClicadoEquacoes (e) {
+function botaoEnterClicadoEquacoes(e) {
     if (e.key === 'Enter') {
-        resolvendoEquacoes ();
+        resolvendoEquacoes();
     }
 }
-opcaoOperacoesEl.addEventListener ('keyup', botaoEnterClicadoEquacoes);
-aEl.addEventListener ('keyup', botaoEnterClicadoEquacoes);
-bEl.addEventListener ('keyup', botaoEnterClicadoEquacoes);
-cEl.addEventListener ('keyup', botaoEnterClicadoEquacoes);
+opcaoOperacoesEl.addEventListener('keyup', botaoEnterClicadoEquacoes);
+aEl.addEventListener('keyup', botaoEnterClicadoEquacoes);
+bEl.addEventListener('keyup', botaoEnterClicadoEquacoes);
+cEl.addEventListener('keyup', botaoEnterClicadoEquacoes);
